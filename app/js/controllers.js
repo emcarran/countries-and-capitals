9b5bf9463f86905c03aca77a291a0228381d39f7
Elizabeth Carranza <elizabeth.m.carranza@gmail.com>
2016-08-17 17:34:38 -0700

    partials complete

---
+++ app/js/controllers.js
@@ -0,0 +1,51 @@
+var app = angular.module('ccApp')
+
+app.controller('mainController', ['$scope', 'countryData', function ($scope, countryData) {}])
+
+app.controller('countriesController', ['$scope', '$location', '$filter', 'countryData', '$q', function ($scope, $location, $filter, countryData, $q) {
+    'use strict';
+
+    var toString = Object.prototype.toString;
+
+    //checking to see if API call returned any data
+    $q.when(countryData.countries).then(function (result) {
+        //checking to see if promise returns an object result
+        if (toString.call(countryData.countries) == '[object Object]') {
+            countryData.countries = result.geonames;
+        }
+        $scope.countries = countryData.countries;
+    });
+
+    $scope.showDetail = function (country) {
+        $location.path('/countries/' + country.countryCode);
+    };
+
+
+    angular.forEach($scope.countries, function (country) {
+        country.areaInSqKm = parseFloat(country.areaInSqKm);
+        country.population = parseFloat(country.population);
+    });
+
+
+  }])
+
+app.controller('detailsController', ['$scope', 'countryData', function ($scope, countryData) {
+
+    countryData.getCountry($route.current.params.countryCode).then(function (result) {
+        $scope.country = result[0];
+        console.log($scope.country);
+
+    });
+    countryData.getCapitals($route.current.params.countryCode).then(function (result) {
+        $scope.capital = result;
+        $scope.capitalPopulation = $scope.capital.population;
+    });
+    countryData.getNeighbors($route.curent.params.countryCode).then(function (result) {
+        $scope.neighbors = result.geo 1 names;
+    });
+
+    //toLowerCase will make flag visible even with lower case country code
+    $scope.flag.$route.current.params.countryCode.toLowerCase();
+    ////toUpperCase will make map visible even with lower case country code
+    $scope.map.$route.current.params.countryCode.toUpperCase();
+}]);
