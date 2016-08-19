be27e21515a0a030bb71625829ea1c6d9e67fbe7
Elizabeth Carranza <elizabeth.m.carranza@gmail.com>
2016-08-19 15:01:32 -0700

    complete with buts

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,21 @@
 var app = angular.module('ccApp', ['DataServices', 'Data', 'ngRoute', 'ngAnimate']);
 
 //inject a $routeProvide and use it to set URL routing rules
-app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
-    $routeProvider
-        .when('/home', {
-            templateUrl: './partials/home.html'
-        })
-        .when('/countries', {
-            templateUrl: './partials/countries.html',
-            controller: 'countriesController'
-        })
-        .when('/countries/:countryCode', {
-            templateUrl: './partials/details.html',
-            controller: 'detailsController'
-        })
-        .otherwise({
-            redirectTo: '/home'
-        });
-
-}]);
+app.config(['$locationProvider', '$routeProvider',
+            function ($locationProvider, $routeProvider) {
+        $routeProvider
+            .when("/home", {
+                templateUrl: "./partials/home.html"
+            })
+            .when("/countries", {
+                templateUrl: "./partials/countries.html",
+                controller: "countriesController",
+            })
+            .when("/countries/:countryCode", {
+                templateUrl: "./partials/details.html",
+                controller: "detailsController",
+            })
+            .otherwise({
+                redirectTo: '/home'
+            });
+            }]);
